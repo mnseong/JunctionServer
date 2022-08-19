a796903db75eee1e593c5246b04aeef6ed884869
seunghun <juniper0917@gmail.com>
2022-08-20 02:53:39 +0900

    websocket build

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 4000;
+var http = require("http").createServer(app);
 
 app.get("/", (req, res) => {
   res.send("ok");
@@ -8,4 +9,18 @@ app.get("/", (req, res) => {
 
 app.listen(port, () => {
   console.log(`server is listening at localhost:${port}`);
+});
+
+const WebSocket = require("ws");
+const wss = new WebSocket.Server({ port: 8080 });
+
+wss.on("connection", function connection(ws, request) {
+  console.log("connection");
+  ws.on("message", function incoming(message) {
+    console.log("received: %s", message);
+  });
+  ws.on("close", function close(code, reason) {
+    console.log("close " + code + ":" + reason);
+  });
+  ws.send("something");
 });
