29a4fd86374cea2e522dd1d536beca026c0ec1bb
seunghun <juniper0917@gmail.com>
2022-08-20 18:18:45 +0900

    stt API Build

--- server.js
+++ server.js
@@ -1,26 +1,51 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 8080;
+const cors = require("cors");
 var http = require("http").createServer(app);
+//const data = new FormData();
+const multer = require("multer");
+const fetch = require("node-fetch");
+const fs = require("fs");
+const request = require("request");
+var upload = multer({ dest: "uploads/" });
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.send("ok");
+app.use(cors());
+
+app.use(express.urlencoded({ extended: true }));
+app.get("/file", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
 
 app.listen(port, () => {
   console.log(`server is listening at localhost:${port}`);
 });
 
-const WebSocket = require("ws");
-const wss = new WebSocket.Server({ port: 8080 });
+app.get("/send", upload.single("fileInput"), (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
 
-wss.on("connection", function connection(ws, request) {
-  console.log("connection");
-  ws.on("message", function incoming(message) {
-    console.log("received: %s", message);
-  });
-  ws.on("close", function close(code, reason) {
-    console.log("close " + code + ":" + reason);
-  });
-  ws.send("something");
+  let speechData = {
+    method: "POST",
+    body: fs.createReadStream("./sttTest1.mp3"),
+    headers: {
+      "Content-Type": "application/octet-stream",
+      "X-NCP-APIGW-API-KEY-ID": "bi3sbu34oo",
+      "X-NCP-APIGW-API-KEY": "3ilzXddSzUs555vWaxIBtg8RCP5n3E9RRvWkYJQ0",
+    },
+  };
+
+  fetch(
+    `https://naveropenapi.apigw.ntruss.com/recog/v1/stt?lang=Kor`,
+    speechData
+  )
+    .then((response) => response.json())
+    .then((response) => {
+      console.log(response);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
